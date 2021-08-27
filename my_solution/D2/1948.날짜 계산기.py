aba6dbc1d01967966d67b958a285e65faad4daba
kidyoo14 <kidyoo14@naver.com>
2021-08-27 13:54:58 +0900

    그래프 문제 추가

--- my_solution/D2/1948.날짜 계산기.py
+++ my_solution/D2/1948.날짜 계산기.py
@@ -1,6 +1,8 @@
 def month_cal(month, day):
-    month_dic = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+    month_dic = {
+        1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
     return_ = 0
+
     for i in month_dic.keys():
         if i < month:
             return_ += month_dic[int(i)]
