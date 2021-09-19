9631cd7f29f1c82300aa8ac0755c1475b511fee8
kidyoo14 <kidyoo14@naver.com>
2021-09-19 16:12:40 +0900

    09.19 2차원 배열 입력 받기 추가

--- 공식/2차원 배열 입력받기.py
+++ 공식/2차원 배열 입력받기.py
@@ -11,3 +11,10 @@ n, m = map(int, input().split())
 my_list = [list(map(int, input().split())) for _ in range(n)]
 
 print(my_list)
+
+#####################
+# 단순 행, 열만 입력 받으려면 이게 제일 간단함
+# 이게 제일 간단함
+
+matrix = [[0 for i in range(3)] for j in range(4)]
+
